43749f068ab479b89545e05cf103d14767e94690
hardzal <rizaldoeta98@gmail.com>
2019-11-18 20:19:53 +0700

    add branch develop

---
+++ README.md
@@ -0,0 +1,3 @@
+# Image Processing Project
+## Created with CamanJs
+
---
+++ src/js/script.js
@@ -0,0 +1,8 @@
+// Caman("#img-sample", function () {
+//     this.brightness(50);
+//     this.contrast(15);
+//     this.render(function () {
+//         alert("Done");
+//     });
+// });
+
